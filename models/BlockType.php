797f6e9bbda150edcbe24fd5890ef9776603938b
Chris <bethel.chris@gmail.com>
2019-05-18 13:38:41 -0400

    move to meta instead of partials

--- models/BlockType.php
+++ models/BlockType.php
@@ -13,7 +13,7 @@ class BlockType extends Content
     /**
      * @var string The container name associated with the model, eg: pages.
      */
-    protected $dirName = 'partials/block-types';
+    protected $dirName = 'meta/block-types';
 
     /**
      * @var bool Wrap code section in PHP tags.
